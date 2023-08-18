9a9d996b7ea52dea7ec3e006767e192e1015af00
ClaireWhere <clairewhere4@gmail.com>
2023-08-18 11:10:27 -0400

    style: add more detailed descriptions

--- utils/message.utils/politics.js
+++ utils/message.utils/politics.js
@@ -16,8 +16,8 @@ module.exports = {
             );
 
     const embed_1 = {
-        title: `‎\n❗Politics Rules❗`,
-        description: `To gain access to the ${politics} channel, please press the button below. This channel contains political content that may be upsetting to some, but please note that all the rules of the server still apply.\n\n**The ${politics} channel is the place to talk about political issues. This is NOT a place to argue politics, but rather to inform and discuss with others current and historical political events/issues. Please respect the following rules for ${politics} as well as the ${rules} of the server.**`,
+        title: ``,
+        description: `# ‎\n# ❗Politics Rules❗\nTo gain access to the ${politics} channel, please press the button below. This channel contains political content that may be upsetting to some, but please note that all the rules of the server still apply on top of the following.\n\n### Disclaimer:\nThe ${politics} channel is the place to talk about political issues. This is **NOT** a place to argue politics, but rather to inform and discuss with others current and historical political events/issues. Please respect the following rules for ${politics} as well as the ${rules} of the server.`,
         color: parseInt(config.colors.light_red.darken[0].hex),
         author: {
           name: `Gender & Sexuality Alliance`,
@@ -41,22 +41,22 @@ module.exports = {
                 "value": `Be calm and understanding of other people's political views. If you disagree, kindly share why you disagree. This is not the place for heated arguments or unproductive back-and-forth disagreements.`
             },
             {
-                "name": `‎\n3️⃣ No name-calling or insulting`,
-                "value": `No matter how extreme someone's views are, please refrain from using slurs or demeaning language toward them. Instead, share what bothers you. Try not to get heated.`
+                "name": `‎\n3️⃣ No name-calling, insulting, or threatening`,
+                "value": `No matter how extreme someone's views are, please refrain from using slurs or demeaning language toward them. Do not insinuate violence. Instead, share what bothers you. Try not to get heated.`
             },
             {
                 "name": `‎\n4️⃣ Avoid escalating fear and distress to your peers`,
                 "value": `Queer people are greatly affected by politics and it can be extremely upsetting, we understand the anger, fear, and pain. Spiraling fear can make it even more upsetting for some, so we ask that you stay composed in discussions. None of us are mental health professionals and there are limits to the amount and type of support you should be receiving in this channel.`
             },
             {
-                "name": `‎\n5️⃣ eBoard may moderate at their discretion`,
-                "value": `If there are loopholes in the rules, it is up to eBoard how to moderate based on what they deem appropriate. eBoard holds the final discretion.`
+                "name": `‎\n5️⃣ Eboard may moderate at their discretion`,
+                "value": `If there are loopholes in the rules, it is up to Eboard how to moderate based on what they deem appropriate. The Eboard holds the final discretion.`
             }
         ]
     };
     const embed_3 = {
-        title: `❗ By agreeing below, you acknowledge that you have read the rules and understand the purpose and appropriate use of the \`\`\`#politics\`\`\` channel.`,
-        description: `You agree that you will continue to be respectful to others and sensitive to everyone's feelings regarding political issues. Please keep the things discussed in the ${politics} channel in this channel only. \n\nThis role may be removed at any time if you are not using this space appropriately and you will be messaged about it. Please note if you are removed, you are not in trouble, we will just not issue warnings for ${politics} as it is a highly sensitive place.\n\nClick this button again to remove ${politics} from your view at any time\n`,
+        title: ``,
+        description: `# ❗ By agreeing below, you acknowledge that you have read the rules and understand the purpose and appropriate use of the ${politics} channel.\n\nYou agree that you will continue to be respectful to others and sensitive to everyone's feelings regarding political issues. Please keep the things discussed in the ${politics} channel in this channel only. \n\nThis role may be removed at any time if you are not using this space appropriately and you will be messaged about it. Please note if you are removed, you are not in trouble, we will just not issue warnings for ${politics} as it is a highly sensitive place.\n\n### Click this button again to remove ${politics} from your view at any time\n`,
         color: parseInt(config.colors.light_red.darken[0].hex),
         timestamp: new Date().toISOString(),
         footer: {
--- utils/message.utils/safe_space.js
+++ utils/message.utils/safe_space.js
@@ -16,8 +16,8 @@ module.exports = {
             );
 
     const embed_1 = {
-        title: `‎\n❗ Safe Space Rules ❗`,
-        description: `To gain access to the ${safe_space} channel, please press the button below. This channel contains heavier topics such as mental health and venting but please note that all the rules of the server still apply. \n\n**This is not a space to argue or talk politics, disagreements should be respectful and absolutely no harassment is tolerated.**`,
+        title: ``,
+        description: `‎\n# ❗ Safe Space Rules ❗\nTo gain access to the ${safe_space} channel, please press the button below. This channel contains heavier topics such as mental health and venting but please note that all the rules of the server still apply on top of the following rules. \n\n### Disclaimer:\nThis is not a space to argue or talk politics, disagreements should be respectful and absolutely no harassment is tolerated.`,
         color: parseInt(config.colors.red.darken[0].hex),
         author: {
           name: `Gender & Sexuality Alliance`,
@@ -38,23 +38,23 @@ module.exports = {
                 "value": `Even though this is the dedicated safe space channel, remember that you still have to be mindful of what others in the safe space are comfortable reading. \n\n>>> **To use spoiler tags**, surround your text with two pipe bar characters (\\|\\|) on each side \\|\\|like this\\|\\|. Your message will appear ||like this||! People will have to click on it before reading. It is also very helpful to include a warning along with your spoiler. \n(Example: Trigger warning: mental health)`
             },
             {
-                "name": `‎\n2️⃣ Please do not include names without explicit permission`,
-                "value": `If you have asked the person you are naming, include in the message that you have permission. Otherwise, the message will be deleted and you'll be messaged to post it again. Referring to someone as “my friend”, “my brother”, etc. is completely acceptable.`
+                "name": `‎\n2️⃣ Please do not include names without permission`,
+                "value": `If a person attends or is affiliated with Oakland University, please do not name them in any negative context unless you have asked permission. Include in the message that you got permission. Do not name any GSA member or anyone otherwise involved in the GSA in a negative context. Otherwise, the message may be deleted and you'll be messaged to post it again with the name removed. Referring to someone as "my friend", "my brother", "somebody" etc. is completely acceptable unless they are in this server or otherwise involved with the GSA.`
             },
             {
                 "name": `‎\n3️⃣ As always, be respectful and welcoming of others`,
-                "value": `Make sure to read and follow the ${rules}. Homophobia, transphobia, sexism, racism, or any type of hate speech, discriminatory, or toxic behavior is **not** tolerated.`
+                "value": `Make sure to read and follow the ${rules}. Homophobia, transphobia, sexism, racism, or any type of hate speech, discriminatory, or toxic behavior is **not** tolerated (talking about your experiences with these things is allowed).`
             },
             {
                 "name": `‎\n4️⃣ Please refrain from sharing heavy emotional or mental health crises`,
-                "value": `${safe_space} is not a place for mental health crises. Please understand that this is not because we don't want you to talk about this, but none of us are mental health professionals and there is only so much we can help. As much as we want to offer you support, there are limits to the type and amount of support you should be receiving.`
+                "value": `${safe_space} is not a place for mental health crises. Please understand this is not because we don't want you to talk about your mental health, but none of us are mental health professionals and there is only so much we can help. As much as we want to offer you support, there are limits to the type and amount of support you should be receiving. Please call or text 988, chat 988lifeline.org or call 911 if you are having a mental heath crisis\n>>> Additionally, keep in mind there may be mandated reporters (i.e. GSC Coordinator, GSC Graduate Assistant, GSC Peer Mentors, etc.) in this space. While they are encouraged to remove themselves from ${safe_space}, they are required by law to report mental health emergencies to the university.`
             }
         ],
     };
 
     const embed_3 = {
-        title: `❗ By agreeing below, you acknowledge that you have read the rules and understand the purpose and appropriate use of the \`\`\`#safe_space\`\`\` channel.`,
-        description: `You agree that you will continue to be respectful to others and sensitive to everyone's feelings. Please keep the things discussed in the ${safe_space} channel private and avoid discussing things outside. \n\nThis role may be removed at any time if you are not using this space appropriately and you will be messaged about it. Please note if you are removed, you are not in trouble, we will just not issue warnings for ${safe_space} as it is a highly sensitive place.\n\nClick this button again to remove ${safe_space} from your view\n‎`,
+        title: ``,
+        description: `## ❗ By agreeing below, you acknowledge that you have read the rules and understand the purpose and appropriate use of the ${safe_space} channel.\nYou agree that you will continue to be respectful to others and sensitive to everyone's feelings. Please keep the things discussed in the ${safe_space} channel private and avoid discussing things outside. \n\nThis role may be removed at any time if you are not using this space appropriately and you will be messaged about it. Please note if you are removed, you are not in trouble, we will just not issue warnings for ${safe_space} as it is a highly sensitive place.\n\n### Click this button again to remove ${safe_space} from your view\n‎`,
         color: parseInt(config.colors.red.darken[0].hex),
         timestamp: new Date().toISOString(),
         footer: {
