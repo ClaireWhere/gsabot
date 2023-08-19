2d20a01725786b69f0ee3a63dd07ff995a021413
ClaireWhere <clairewhere4@gmail.com>
2023-08-19 12:27:54 -0400

    add debugging information

--- client/events/ready.js
+++ client/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'ready', // the event that this file is for
-    once: true, // specifies this event should only run once
+    name: 'ready',
+    once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
