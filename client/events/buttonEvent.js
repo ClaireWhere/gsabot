2a2ec5c2a771121fa9e5381981129923035cf6b5
ClaireWhere <clairewhere4@gmail.com>
2023-08-23 19:16:01 -0400

    feat: add utils/roles.utils/roles.js
    move role utility functionality to one location to be accessed by other processes
    
    Add the following functions:
    + findRole(interaction, role_name)
    + removeExclusive(interaction, id)
    + memberHasRole(interaction, role_name)
    + addRole(interaction, role)
    + addRoleByName(interaction, role_name)
    + addFormatRole(interaction, role_name)
    +  removeRole(interaction, role)
    + removeRoleByName(interaction, role_name)
    + removeFormatRole(interaction, role_name)
    + toggleRole(interaction, role_name)
    + getPermissionsFromArray(array)

--- client/events/buttonEvent.js
+++ client/events/buttonEvent.js
@@ -1,9 +1,8 @@
-'use strict';
-const { Client, GatewayIntentBits, Events } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { Events } = require('discord.js');
 
 const { config } = require('../config.json');
 const { color_handler } = require('../../utils/colorRoles.js');
+const { toggleRole } = require('../../utils/roles.utils/roles');
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -69,76 +68,8 @@ module.exports = {
             return false;
         }
         
-        if (await memberHasRole(interaction, role_name)) {
-            await removeRole(interaction, role_name);
-            return true;
-        } else {
-            await addRole(interaction, role_name);
-            await removeExclusive(interaction, id);
-        }
+        await toggleRole(interaction, role_name);
         return true;
     },
 };
 
-async function removeExclusive(interaction, id) {
-    if (id.length == 0) { return false; }
-    
-    var role_id = id[0];
-    var root_element = config.roles;
-    if (id.length > 1) {
-        role_id = id[1];
-        root_element = config.roles[id[0]];
-    }
-
-    const exclusive = root_element[role_id].exclusion;
-    if (exclusive == undefined || exclusive.length == 0) {
-        return true;
-    }
-
-    await exclusive.forEach(async element => {
-        try {
-            if (await memberHasRole(interaction, root_element[element].name)) {
-                await removeRole(interaction, root_element[element].name);
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    });
-}
-
-async function memberHasRole(interaction, role_name) {
-    return await interaction.member.roles.cache.some(role => role.name === role_name);
-}
-
-async function addRole(interaction, role_name) {
-    try {
-        var role = await interaction.guild.roles.cache.find(role => role.name === role_name);
-        if (!role) { return false; }
-        await interaction.member.roles.add(role);
-    } catch (error) {
-        console.error(error);
-        await interaction.followUp({ephemeral: true, content: `Something went wrong when adding your role!`});
-        return false;
-    }
-    
-    if (role_name == 'GSA Member') {
-        await interaction.followUp({ephemeral: true, content: `🥳🥳 You are now a officially a part of the GSA Discord server!`});
-    } else {
-        await interaction.followUp({ephemeral: true, content: `You now have the ${role} role!`});
-    }
-    
-    return true;
-}
-
-async function removeRole(interaction, role_name) {
-    try {
-        var role = interaction.guild.roles.cache.find(role => role.name === role_name);
-        await interaction.member.roles.remove(role);
-    } catch (error) {
-        console.error(error);
-        await interaction.followUp({ephemeral: true, content: `Something went wrong when adding your role!`});
-        return false;
-    }
-    await interaction.followUp({ephemeral: true, content: `You no longer have the ${role} role!`});
-    return true;
-}
