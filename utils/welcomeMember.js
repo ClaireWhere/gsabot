c827d9c747e9e8c58ad6a3f347846b648e635755
ClaireWhere <clairewhere4@gmail.com>
2023-08-23 19:19:40 -0400

    refactor: redistribute welcome member utility
    move welcome member functionality to its own utility script for cleaner organization

---
+++ utils/welcomeMember.js
@@ -0,0 +1,25 @@
+
+
+async function welcomeMember(interaction) {
+    if (await memberHasRole(interaction, "GSA Member")) {
+        await interaction.followUp({ephemeral: true, content: `${await interaction.guild.emojis.cache.find(emoji => emoji.name === 'rich_thinking') ?? '🤔'} There was an error. You are already a member!`});
+        return false;
+    } else {
+        await addFormatRole(interaction, "GSA Member");
+        const color = config.colors.rainbow.at(Math.random()*config.colors.rainbow.length).hex;
+        const introductions = await interaction.guild.channels.cache.find(channel => channel.name === 'introductions');
+        const roles = await interaction.guild.channels.cache.find(channel => channel.name === 'roles');
+        const welcome = await interaction.guild.channels.cache.find(channel => channel.name === 'welcome');
+        await welcome.send({ content: `${interaction.member}`, embeds: [{title: ``, description: `# Welcome to the GSA Discord Server!\nEveryone say hi to ${interaction.member}! Feel free to introduce yourself in ${introductions} and choose your ${roles}`, color: parseInt(color)}] })
+            .then(message => {
+                message.react('👋');
+                console.log(`Successfully sent welcome message`);
+            })
+            .catch(`There was an error sending message welcome message : ${console.error}`);
+        
+    }
+    return true;
+}
+
+
+module.exports = { welcomeMember }
