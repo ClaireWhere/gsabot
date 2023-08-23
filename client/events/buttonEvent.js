cd1e694b761e2e17e30c76636d8c5bce6f448015
ClaireWhere <clairewhere4@gmail.com>
2023-08-23 19:19:40 -0400

    refactor: redistribute welcome member utility
    move welcome member functionality to its own utility script for cleaner organization

--- client/events/buttonEvent.js
+++ client/events/buttonEvent.js
@@ -3,6 +3,7 @@ const { Events } = require('discord.js');
 const { config } = require('../config.json');
 const { color_handler } = require('../../utils/colorRoles.js');
 const { toggleRole } = require('../../utils/roles.utils/roles');
+const { welcomeMember } = require('../../utils/welcomeMember');
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -22,25 +23,8 @@ module.exports = {
                     return;
                 });
 
-        if (interaction.customId == 'member') {
-            if (await memberHasRole(interaction, "GSA Member")) {
-                await interaction.followUp({ephemeral: true, content: `${await interaction.guild.emojis.cache.find(emoji => emoji.name === 'rich_thinking') ?? '🤔'} There was an error. You are already a member!`});
-                return false;
-            } else {
-                await addRole(interaction, "GSA Member");
-                const color = config.colors.rainbow.at(Math.random()*config.colors.rainbow.length).hex;
-                const introductions = await interaction.guild.channels.cache.find(channel => channel.name === 'introductions');
-                const roles = await interaction.guild.channels.cache.find(channel => channel.name === 'roles');
-                const welcome = await interaction.guild.channels.cache.find(channel => channel.name === 'welcome');
-                await welcome.send({ content: `${interaction.member}`, embeds: [{title: ``, description: `# Welcome to the GSA Discord Server!\nEveryone say hi to ${interaction.member}! Feel free to introduce yourself in ${introductions} and choose your ${roles}`, color: parseInt(color)}] })
-                    .then(message => {
-                        message.react('👋');
-                        console.log(`Successfully sent welcome message`);
-                    })
-                    .catch(`There was an error sending message welcome message : ${console.error}`);
-                
-            }
-            return true;
+        if (interaction.customId === 'member') {
+            return await welcomeMember(interaction);
         }
 
         // categorized button id's take the form parent:child. Eg. pronouns:she_her is part of the pronouns category with the child id being she_her. See more in config.json
