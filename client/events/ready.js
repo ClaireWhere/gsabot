dbf243dcc20d47abe1015b0b8423b5d5535c605b
ClaireWhere <clairewhere4@gmail.com>
2023-08-26 12:29:09 -0400

    feat: add custom status

--- client/events/ready.js
+++ client/events/ready.js
@@ -1,7 +1,24 @@
+const { Client, ActivityType } = require("discord.js");
+const { config } = require('../config.json');
+
 module.exports = {
     name: 'ready',
     once: true,
+    /**
+     * 
+     * @param {Client} client 
+     */
     execute(client) {
+        if (config.custom_status.enabled) {
+            client.user.setPresence({
+                activities: [{
+                    name: config.custom_status.name,
+                    type: ActivityType.Custom,
+                    state: config.custom_status.status,
+                }]
+            })
+        }
+        
         console.log(`Ready! Logged in as ${client.user.tag}`);
     },
 };
