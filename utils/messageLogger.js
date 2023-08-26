1910bae75c3e39b7b7cafad2527d9ce6d8cd0871
ClaireWhere <clairewhere4@gmail.com>
2023-08-26 12:26:56 -0400

    style: remove commented out code

--- utils/messageLogger.js
+++ utils/messageLogger.js
@@ -92,76 +92,6 @@ function select(query) {
 
 module.exports = { insertMessageLog, getMessageLog, verifyMessageId }
 
-// async function run(cb, ...args) {
-//     try {
-//         await client.connect();
-//         console.log(`connection formed with ${uri}`);
-//         return await cb(client.db(), args);
-//     } catch (err) {
-//         console.error(err);
-//     } finally {
-//         await client.close();
-//         console.log(`connection with ${uri} successfully closed`);
-//     }
-// }
-
-// /**
-//  * 
-//  * @param {LoggedMessage} messageData 
-//  * @returns 
-//  */
-// async function insertMessageLog(messageData) {
-//     return await run(insert, messageData);
-// };
-
-// /**
-//  * 
-//  * @param {number} id 
-//  * @returns LoggedMessage object with the provided id
-//  */
-// async function findMessageLog(id) {
-//     return await run(find, id);
-// }
-
-// /**
-//  * 
-//  * @param {Db} db 
-//  * @param {LoggedMessage} messageData 
-//  * @returns 
-//  */
-// async function insert(db, messageData) {
-//     return db.collection(collection).insertOne(messageData[0].messageData)
-//         .then(() => {
-//             console.log(`inserted into ${collection}`);
-//             return true;
-//         }).catch(err => {
-//             console.error(err);
-//             return false;
-//         });
-
-// }
-
-// /**
-//  * 
-//  * @param {Db} db 
-//  * @param {number} id 
-//  * @returns LoggedMessage object with the provided id
-//  */
-// async function find(db, id) {
-//     return await db.collection(collection).findOne({id: id[0]})
-//         .then((data) => {
-//             if (isEmpty(data)) {
-//                 console.log(`no data found in ${collection} for ${id}`);
-//                 return null;
-//             }
-//             console.log(`retrieved ${id} from ${collection}`);
-//             return LoggedMessage.logFromJson(data);
-//         }).catch((err) => {
-//             console.error(err);
-//             return null;
-//         });
-// }
-
 /**
  * Filters out some invalid message id's. Does not accurately verify message id's as true all of the time.
  * 
@@ -171,16 +101,3 @@ module.exports = { insertMessageLog, getMessageLog, verifyMessageId }
 function verifyMessageId(id) {
     return (id.match('^[0-9]*$') && id.length >= 17) ?? false;
 }
-
-// function isEmpty(obj) {
-//     if (obj === undefined) { return true; }
-//     for(var prop in obj) {
-//         if(obj.hasOwnProperty(prop))
-//             return false;
-//     }
-//     return true;
-// }
-
-
-
-// module.exports = { insertMessageLog, findMessageLog, verifyMessageId }
