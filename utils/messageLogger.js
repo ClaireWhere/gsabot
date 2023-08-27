a30d5c210e14cd3122b6dbf576d746fca01ef911
ClaireWhere <clairewhere4@gmail.com>
2023-08-27 14:52:56 -0400

    perf: only load database when it's needed
    + add open function to open a connection with the database
    + add getDbDirectory function to get the database directory even when loaded as a module from another path
    + surround row select in try catch to catch invalid result or result that doesn't exist
    + fix: move WHERE in select to end of statement for proper SQL
    + close the database after use

--- utils/messageLogger.js
+++ utils/messageLogger.js
@@ -1,18 +1,20 @@
 const { LoggedMessage } = require('../models/LoggedMessage');
 const { config } = require('../config.json');
 const { debug } = require('../utils/debugger');
-
-const db = require('better-sqlite3')(`${config.database.name}.db`, {
-    timeout: 2000
-});
-db.pragma('journal_mode = WAL');
-
 /**
  * 
  * @param {LoggedMessage} message 
  * @returns 
  */
 function insertMessageLog(message) {
+    if (!message) { return; }
+    const db = open({
+        timeout: 2000,
+        fileMustExist: true
+    });
+
+    if (!db) { return false; }
+
     try {
         const insert_user = db.prepare('INSERT INTO user (id, name) VALUES (?, ?)');
         insert_user.run(message.author, message.author_name);
@@ -40,39 +42,69 @@ function insertMessageLog(message) {
         debug('[SQLite] inserted into deleted_message');
     } catch (error) {
         debug('[SQLite] did not insert into deleted_message', error);
+        db.close();
         return false;
     }
+    db.close();
     return true;
 }
 
 /**
  * 
  * @param {number} message_id - the id property of the deleted_message record to retrieve from
- * @returns {LoggedMessage} LoggedMessage object containing the information from the deleted_message record
+ * @returns {LoggedMessage | null} LoggedMessage object containing the information from the deleted_message record
  */
 function getMessageLog(message_id) {
-    const select_log = db.prepare(`
-    SELECT 
-        message.id, message.content, message.date, deleted_message.deleted_on, user.id, user.name, channel.id, channel.name, message.guild
-        FROM deleted_message
-        WHERE deleted_message.id = @message_id
-            LEFT JOIN message
-                ON deleted_message.id = @message_id
-            LEFT JOIN user
-                ON message.author=user.id
-            LEFT JOIN channel
-                ON message.channel=channel.id
-    `);
-    const result = select_log.expand().get({message_id: message_id});
-    return new LoggedMessage(result.message.content, result.message.id, result.user.id, result.channel.id, result.channel.name, result.user.name, result.message.date, result.message.guild);
-}
+    if (!verifyMessageId(message_id)) { return null; }
 
+    const db = open({
+        timeout: 2000,
+        fileMustExist: true,
+        readonly: true
+    });
+
+    if (!db) { return false; }
+    
+    try {
+        const select_log = db.prepare(`
+        SELECT 
+            message.id, message.content, message.guild, message.date, deleted_message.deleted_on, user.id, user.name, channel.id, channel.name
+            FROM message
+                LEFT JOIN deleted_message
+                    ON deleted_message.message_id = message.id
+                LEFT JOIN user
+                    ON message.author=user.id
+                LEFT JOIN channel
+                    ON message.channel=channel.id
+            WHERE message.id = @message_id;
+        `);
+        const result = select_log.expand().get({message_id: message_id});
+        db.close();
+        return new LoggedMessage(result.message.id, result.message.content, result.user.id, result.user.name, result.channel.id, result.channel.name, result.message.guild, result.message.date, result.deleted_message.deleted_on);
+    } catch (error) {
+        console.error(error);
+    }
+    db.close();
+    return null;
 }
 
 /**
  * 
+ * @param {import('better-sqlite3').Options} options 
+ * @returns 
  */
+function open(options) {
+    try {
+        const db = require('better-sqlite3')(`${getDbDirectory()}${config.database.name}.db`, options);
+        db.pragma('journal_mode = WAL');
+        return db;
+    } catch (error) {
+        debug(`${config.database.name}.db does not exist! Run "npm run initialize" to initialize the database`);
+        return undefined;
+    }
 }
+
+
 
 module.exports = { insertMessageLog, getMessageLog, verifyMessageId }
 
@@ -84,4 +116,8 @@ module.exports = { insertMessageLog, getMessageLog, verifyMessageId }
  */
 function verifyMessageId(id) {
     return (id.match('^[0-9]*$') && id.length >= 17) ?? false;
+}
+
+function getDbDirectory() {
+    return __dirname.slice(0, __dirname.lastIndexOf('utils'));
 }
