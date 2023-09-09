9d56015e99f623504305332344f29121b7f6b21b
ClaireWhere <clairewhere4@gmail.com>
2023-09-09 13:08:50 -0400

    refactor: move getDate function to utils

--- utils/utils.js
+++ utils/utils.js
@@ -121,4 +121,14 @@ function getChannelParentName(channel) {
     return channel.parent.name.toLowerCase();
 }
 
-module.exports = { arrayMatch, isEmpty, formatList, removeIntersection, arrayToLowerCase, toNum, isNumeric, getChannelParentName }
+function getDate() {
+    var date = new Date();
+    return `${date.getFullYear()}-${pad(date.getMonth(), 2)}-${pad(date.getDay(), 2)} ${pad(date.getHours(), 2)}:${pad(date.getMinutes(), 2)}`;
+}
+function pad(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
+
+module.exports = { arrayMatch, isEmpty, formatList, removeIntersection, arrayToLowerCase, toNum, isNumeric, getChannelParentName, getDate }
