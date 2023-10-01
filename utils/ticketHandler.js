13e806b3a14b85e320e078589ca840208a2eb6a1
ClaireWhere <clairewhere4@gmail.com>
2023-09-30 20:38:33 -0400

    feat: add ticket handler

---
+++ utils/ticketHandler.js
@@ -0,0 +1,26 @@
+const { config } = require('../client/config.json');
+const { logger } = require('./logger');
+/**
+ * 
+ * @param {import('discord.js').Interaction} interaction 
+ * @param {string} id
+ */
+async function ticketDisplayHandler(interaction, id) {
+}
+
+/**
+ * 
+ * @param {import('discord.js').Interaction} interaction 
+ * @param {string} id
+ */
+async function ticketSubmitHandler(interaction) {
+}
+
+/**
+ * 
+ * @param {import('discord.js').Interaction} interaction 
+ */
+async function gscAnnouncementHandler(interaction) {
+}
+
+module.exports = { ticketDisplayHandler, ticketSubmitHandler }
