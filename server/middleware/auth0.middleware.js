449d870781f7b79f7458659ba346e75e00eefcb2
ClaireWhere <clairewhere4@gmail.com>
2023-10-03 21:55:53 -0400

    feat: add authentication middleware

---
+++ server/middleware/auth0.middleware.js
@@ -0,0 +1,39 @@
+const {
+    auth,
+    claimCheck,
+    InsufficientScopeError,
+} = require("express-oauth2-jwt-bearer");
+const dotenv = require("dotenv");
+  
+dotenv.config();
+  
+const validateAccessToken = auth({
+    issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+    audience: process.env.AUTH0_AUDIENCE,
+    tokenSigningAlg: process.env.AUTH0_SIGNING
+});
+  
+const checkRequiredPermissions = (requiredPermissions) => {
+    return (req, res, next) => {
+      const permissionCheck = claimCheck((payload) => {
+        const permissions = payload.permissions || [];
+  
+        const hasPermissions = requiredPermissions.every((requiredPermission) =>
+          permissions.includes(requiredPermission)
+        );
+  
+        if (!hasPermissions) {
+          throw new InsufficientScopeError();
+        }
+  
+        return hasPermissions;
+      });
+  
+      permissionCheck(req, res, next);
+    };
+};
+  
+module.exports = {
+    validateAccessToken,
+    checkRequiredPermissions,
+};
