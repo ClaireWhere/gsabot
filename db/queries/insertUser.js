9d2d2cab2b69755f209f22988b912b6fbdb51df5
ClaireWhere <clairewhere4@gmail.com>
2024-05-06 11:16:23 -0400

    fix(db): use updated user schema

--- db/queries/insertUser.js
+++ db/queries/insertUser.js
@@ -1,8 +1,8 @@
 const db = require('../db');
 
-function insertUser(id, username, discriminator, avatar, isBot) {
-    const query = `INSERT INTO users (id, username, discriminator, avatar, is_bot) VALUES ($1, $2, $3, $4, $5)`;
-    const values = [id, username, discriminator, avatar, isBot];
+function insertUser(id, username, displayName, avatar) {
+    const query = `INSERT INTO users (id, username, display_name, avatar) VALUES ($1, $2, $3, $4)`;
+    const values = [id, username, displayName, avatar];
     return db.query(query, values, (queryError, queryResponse) => {
         if (queryError) {
             console.error(queryError.stack);
