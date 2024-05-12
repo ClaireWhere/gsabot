2b9c8fa8232876feb5d0db311d38cd17bb3ebac2
ClaireWhere <clairewhere4@gmail.com>
2024-05-11 22:06:07 -0400

    feat(server): update to postgres database

--- server/index.js
+++ server/index.js
@@ -1,17 +1,170 @@
 const express = require('express');
-const { getMessageLog } = require('../utils/db.utils/messageLogger');
-const { logger } = require('../utils/logger');
+const getMessage = require('./db/queries/getMessage');
+const { getEdits } = require('./db/queries/getMessageEdit');
+const { getDeletedMessage } = require('./db/queries/getDeletedMessage');
+const { logger } = require('./utils/logger');
 
-require('dotenv').config();
-const PORT = process.env.APP_PORT || 8080;
+const DEFAULT_PORT = 8080;
+const PORT = DEFAULT_PORT;
 
-var app = express();
+const app = express();
 
-app.get('/logs/*', async function(req, res) {
-    const id = req.path.split('/')[2];
-    var m = getMessageLog(id);
-    logger.info(`[RESPONSE] ` + (m != null ? `retrieved data for ${id}` : `no log found for ${id}`))
-    res.send(!m ? `no log found for ${id}.` : m.content);
+app.get('/', (req, res) => {
+    res.send('Welcome to the gsabot API!');
+});
+
+app.get('/deleted/*/raw', (req, res) => {
+    const id = req.query.id;
+    const m = getDeletedMessage(id);
+    logger.info(`[RESPONSE] ${m ? `retrieved deleted_message data for ${id}` : `no record found in deleted_message for ${id}`}`);
+    
+    if (!m) {
+        res.send(`no log found for ${id}.`);
+        return;
+    }
+
+    const {
+        most_recent_content: mostRecentContent,
+    } = m;
+
+    res.send(mostRecentContent);
+});
+
+app.get('/deleted/*', (req, res) => {
+    const id = req.query.id;
+    const m = getDeletedMessage(id);
+    logger.info(`[RESPONSE] ${m ? `retrieved deleted_message data for ${id}` : `no record found in deleted_message for ${id}`}`);
+    
+    if (!m) {
+        res.send(`no log found for ${id}.`);
+        return;
+    }
+
+    const {
+        most_recent_content: mostRecentContent,
+        most_recent_edit_at: mostRecentEditAt,
+        deleted_at: deletedAt,
+        created_at: createdAt,
+        author_id: authorId,
+        author_nickname: authorNickname,
+        author_name: authorName,
+        author_display_name: authorDisplayName,
+        author_avatar: authorAvatar,
+        channel_name: channelName,
+        guild_name: guildName
+    } = m;
+
+    const responseString = `
+        most_recent_content: ${mostRecentContent}
+        most_recent_edit_at: ${mostRecentEditAt}
+        deleted_at: ${deletedAt}
+        created_at: ${createdAt}
+        author_id: ${authorId}
+        author_nickname: ${authorNickname}
+        author_name: ${authorName}
+        author_display_name: ${authorDisplayName}
+        author_avatar: ${authorAvatar}
+        channel_name: ${channelName}
+        guild_name: ${guildName}
+    `;
+    res.send(responseString);
+});
+
+app.get('/message/*/raw', (req, res) => {
+    const id = req.query.id;
+    const m = getMessage(id);
+    logger.info(`[RESPONSE] ${m ? `retrieved message data for ${id}` : `no record found in message for ${id}`}`);
+    
+    if (!m) {
+        res.send(`no log found for ${id}.`);
+        return;
+    }
+
+    const {
+        content,
+    } = m;
+
+    res.send(content);
+});
+
+app.get('/message/*', (req, res) => {
+    const id = req.query.id;
+    const m = getMessage(id);
+    logger.info(`[RESPONSE] ${m ? `retrieved message data for ${id}` : `no record found in message for ${id}`}`);
+
+    if (!m) {
+        res.send(`no log found for ${id}.`);
+        return;
+    }
+
+    const {
+        content,
+        created_at: createdAt,
+        author_id: authorId,
+        author_nickname: authorNickname,
+        author_name: authorName,
+        author_display_name: authorDisplayName,
+        author_avatar: authorAvatar,
+        channel_name: channelName,
+        guild_name: guildName
+    } = m;
+
+    const responseString = `
+        content: ${content}
+        created_at: ${createdAt}
+        author_id: ${authorId}
+        author_nickname: ${authorNickname}
+        author_name: ${authorName}
+        author_display_name: ${authorDisplayName}
+        author_avatar: ${authorAvatar}
+        channel_name: ${channelName}
+        guild_name: ${guildName}
+    `;
+    res.send(responseString);
+});
+
+app.get('/message/*/edit', (req, res) => {
+    const id = req.query.id;
+    const m = getEdits(id);
+    logger.info(`[RESPONSE] ${m ? `retrieved message_edit data for ${id}` : `no record found in message_edit for ${id}`}`);
+
+    if (!m) {
+        res.send(`no log found for ${id}.`);
+        return;
+    }
+
+    const responseMessage = [];
+    m.array.forEach(element => {
+        const {
+            content,
+            edited_at: editedAt,
+            original_content: originalContent,
+            attachment,
+            created_at: createdAt,
+            author_nickname: authorNickname,
+            author_name: authorName,
+            author_display_name: authorDisplayName,
+            author_avatar: authorAvatar,
+            channel_name: channelName,
+            guild_name: guildName
+        } = element;
+
+        responseMessage.push(`
+            content: ${content}
+            edited_at: ${editedAt}
+            original_content: ${originalContent}
+            attachment: ${attachment}
+            created_at: ${createdAt}
+            author_nickname: ${authorNickname}
+            author_name: ${authorName}
+            author_display_name: ${authorDisplayName}
+            author_avatar: ${authorAvatar}
+            channel_name: ${channelName}
+            guild_name: ${guildName}
+        `);
+    });
+
+    res.send(responseMessage.join('\n'));
 });
 
 app.listen(PORT, () => {
