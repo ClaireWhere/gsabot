f3c5e58315a8a8f4e5a64fd4bf0ed43e19e228a6
ClaireWhere <clairewhere4@gmail.com>
2024-05-11 20:25:13 -0400

    fix: add VARCHAR type to table ids

--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS channel (
-    id PRIMARY KEY,
+    id VARCHAR(20) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS user (
-    id PRIMARY KEY,
+    id VARCHAR(20) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     display_name VARCHAR(255) NOT NULL,
     avatar VARCHAR(255) DEFAULT NULL,
@@ -13,15 +13,15 @@ CREATE TABLE IF NOT EXISTS user (
 );
 
 CREATE TABLE IF NOT EXISTS guild (
-    id PRIMARY KEY,
+    id VARCHAR(20) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS guild_user (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    guild_id INT NOT NULL,
+    user_id VARCHAR(20) NOT NULL,
+    guild_id VARCHAR(20) NOT NULL,
     nickname VARCHAR(255) DEFAULT NULL,
     display_hex_color VARCHAR(7) DEFAULT NULL,
     FOREIGN KEY (user_id) REFERENCES user(id),
@@ -29,18 +29,18 @@ CREATE TABLE IF NOT EXISTS guild_user (
 );
 
 CREATE TABLE IF NOT EXISTS message (
-    id PRIMARY KEY,
+    id VARCHAR(20) PRIMARY KEY,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    guild_user_id INT NOT NULL,
-    channel_id INT NOT NULL,
+    guild_user_id INTEGER NOT NULL,
+    channel_id VARCHAR(20) NOT NULL,
     FOREIGN KEY (guild_user_id) REFERENCES guild_user(id),
     FOREIGN KEY (channel_id) REFERENCES channel(id),
 );
 
 CREATE TABLE IF NOT EXISTS attachment (
     id SERIAL PRIMARY KEY,
-    message_id INT NOT NULL,
+    message_id VARCHAR(20) NOT NULL,
     url VARCHAR(255) NOT NULL,
     content_type VARCHAR(255) NOT NULL,
     name VARCHAR(255) DEFAULT NULL,
@@ -50,14 +50,14 @@ CREATE TABLE IF NOT EXISTS attachment (
 
 CREATE TABLE IF NOT EXISTS deleted_message(
     id SERIAL PRIMARY KEY,
-    message_id INT NOT NULL UNIQUE,
+    message_id VARCHAR(20) NOT NULL UNIQUE,
     deleted_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (message_id) REFERENCES message(id)
 );
 
 CREATE TABLE IF NOT EXISTS message_edit(
     id SERIAL PRIMARY KEY,
-    message_id INT NOT NULL,
+    message_id VARCHAR(20) NOT NULL,
     content TEXT NOT NULL,
     edited_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (message_id) REFERENCES message(id)
