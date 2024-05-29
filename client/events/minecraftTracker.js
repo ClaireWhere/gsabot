35d64aa59044f714e25b17c9b197c3a03fecbd5e
ClaireWhere <clairewhere4@gmail.com>
2024-05-28 20:45:25 -0400

    feat(client): add additional MC server logging

--- client/events/minecraftTracker.js
+++ client/events/minecraftTracker.js
@@ -19,6 +19,20 @@ async function setStatusChannel(status) {
     await channel.setName(status);
 }
 
+function parseServerInfo(response) {
+    let serverInfo = '';
+    if (response?.data?.version?.name) {
+        serverInfo += `\n\tVersion: ${response.data.version.name}`;
+    }
+    if (response?.data?.latency) {
+        serverInfo += `\n\tLatency: ${response.data.latency}`;
+    }
+    if (response?.data?.players?.sample?.length > 0) {
+        serverInfo += `\n\tPlayers: ${response.data.players.sample.map(player => {return player.name}).join(', ')}`;
+    }
+    return serverInfo;
+}
+
 async function checkServer() {
 
     const ip = `${process.env.MINECRAFT_SUBDOMAIN}.${process.env.DOMAIN}`;
@@ -28,23 +42,23 @@ async function checkServer() {
         .then(async response => {
             const playersOnline = response?.data?.players?.online;
             if (playersOnline === previousStatus) {
-                logger.debug(`[${ip}] no changes since last check`);
+                logger.debug(`[${ip}] no changes since last check${parseServerInfo(response)}`);
                 return;
             }
 
             if (typeof playersOnline === 'number') {
                 previousStatus = playersOnline;
-                await setStatusChannel(`${previousStatus} Player${previousStatus === 1 ? '' : 's'} Online`).catch(error => {return logger.warn(`[${ip}] there was an error fetching the server status channel (${error})`)});
-                logger.info(`[${ip}] ${previousStatus} player${previousStatus === 1 ? '' : 's'} online`);
+                const statusMessage = `${previousStatus} Player${previousStatus === 1 ? '' : 's'} Online`
+                await setStatusChannel(statusMessage).catch(error => {return logger.warn(`[${ip}] there was an error fetching the server status channel (${error})`)});
+                logger.debug(`[${ip}] ${statusMessage}${parseServerInfo(response)}`);
             } else if (previousStatus === 'warning') {
                 logger.debug(`[${ip}] no changes since last check`);
             } else {
                 previousStatus = 'warning';
                 await setStatusChannel(`Server Offline :(`).catch(error => {return logger.warn(`[${ip}] there was an error fetching the server status channel (${error})`)});
-                logger.warn(`[${ip}] could not reach server!`);
+                logger.warn(`[${ip}] could not reach server! (${response?.data?.error})`);
             }
-        })
-        .catch(error => {
+        }).catch(error => {
             if (previousStatus === 'error') {
                 logger.debug(`[${[ip]}] no changes since last check`);
             } else {
