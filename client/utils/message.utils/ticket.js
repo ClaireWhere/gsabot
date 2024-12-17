165d945c8481a77b09814938245f052dd461c4bc
ClaireWhere <clairewhere4@gmail.com>
2024-12-16 23:29:01 -0500

    feat: add debug logging

--- client/utils/message.utils/ticket.js
+++ client/utils/message.utils/ticket.js
@@ -1,5 +1,6 @@
 const { config } = require('../../config.json');
 const { ButtonStyle, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { logger } = require('../logger');
 
 
 const SUPPORT_TICKET_ID = 'support_ticket';
@@ -17,16 +18,18 @@ function getTicketTypeID(type) {
 }
 
 module.exports = { 
-    execute(interaction) {
+    async execute(interaction) {
+        logger.debug(`Ticket command received with options: {type: ${interaction.options.getString('type')}, title: ${interaction.options.getString('title')}, description: ${interaction.options.getString('description')}, label: ${interaction.options.getString('label')}, emoji: ${interaction.options.getString('emoji')}`);
         const button = new ButtonBuilder()
             .setStyle(ButtonStyle.Danger)
             .setLabel(interaction.options.getString('label'))
             .setCustomId(`ticket:${getTicketTypeID(interaction.options.getString('type'))}`)
             .setEmoji(interaction.options.getString('emoji'))
             .setDisabled(false);
-        
+        logger.debug(`Ticket button created`);
         const row = new ActionRowBuilder()
             .addComponents(button);
+        logger.debug(`Ticket button row created`);
 
         const embed = {
             title: interaction.options.getString('title'),
@@ -42,8 +45,10 @@ module.exports = {
             },
             color: parseInt(Number(config.colors.light_red.darken[0].hex), 10)
         }
+        logger.debug(`Ticket embed created`);
 
         const ticket = {embeds: [embed], components: [row] };
+        logger.debug(`Ticket message created`);
         return ticket;
     }
  }
