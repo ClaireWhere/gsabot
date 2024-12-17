12f2ce01b06f6318186fcb55f88b9e3a67023622
ClaireWhere <clairewhere4@gmail.com>
2024-12-16 22:49:58 -0500

    fix #73: Remove cast hex string to Number

--- client/utils/roles.utils/colorRoles.js
+++ client/utils/roles.utils/colorRoles.js
@@ -1,5 +1,6 @@
 const { logger } = require('../logger');
 const { getBotRolePosition } = require('./roles');
+const { isValidHexColor } = require('../utils);
 
 
 /**
@@ -29,19 +30,24 @@ async function colorHandler(interaction, id) {
     if (id.length === 0) { 
         return await cancelInteraction(interaction, '. There was an error! It looks like the button you clicked was invalid 🤔\nTry `/set color` again');
     }
+    
     if (id === 'no') {
         await cancelInteraction(interaction, '');
         return true;
     }
+
+    if (!isValidHexColor(id)) {
+        return await.editReply({content: 'There was an error! The hex color specified is not a valid color...\nTry `/set color` again'});
+    }
     
     // Color value of 0 specifies no/default color in discord, but we want it to be fully black (like #000000), so we make it like #000001 instead
-    const hex = parseInt(Number(id), 16) === 0 ? 1 : parseInt(Number(id), 16);
+    const hex = parseInt(id, 16) === 0 ? 1 : parseInt(id, 16);
 
     const roleName = `${interaction.member.nickname ?? interaction.member.displayName ?? interaction.member.id}'s Color`;
 
     const memberRole = await interaction.member.roles.cache.find(role => {return role.name.endsWith(`'s Color`)});
 
-    if (memberRole === undefined) {        
+    if (memberRole === undefined) {
         await interaction.guild.roles.create( {name: roleName, color: hex, permissions: [], position: await getGreatestRolePosition(interaction)} )
             .then(async role => {
                     await interaction.member.roles.add(role);
