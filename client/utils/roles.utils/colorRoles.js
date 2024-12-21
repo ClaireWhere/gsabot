484276578c8724efd98da08f940bdf4849cb57c5
ClaireWhere <clairewhere4@gmail.com>
2024-12-21 13:29:15 -0500

    feat: select new role position based on others
    Select the new role position based on the hierarchy of roles with priority given to the defined color roles, then the highest colored role, and ultimately falling back on the highest possible position to ensure the color displays

--- client/utils/roles.utils/colorRoles.js
+++ client/utils/roles.utils/colorRoles.js
@@ -1,26 +1,61 @@
 const { logger } = require('../logger');
 const { getBotRolePosition } = require('./roles');
-const { isValidHexColor } = require('../utils);
+const { isValidHexColor } = require('../utils');
+
+/**
+ * Get the lowest positioned defined color role
+ * @param {import('discord.js').Interaction} interaction 
+ * @returns {number | undefined} The position of the lowest color role or undefined if no color roles exist
+ */
+function getLowestColorRolePosition(interaction) {
+    let pos;
+    interaction.guild.roles.cache.forEach((role) => {
+        if (role.name.endsWith(`'s Color`) && (role.position < pos || pos === undefined)) {
+            pos = role.position;
+        }
+    });
+    return pos;
+}
+
+/**
+ * Get the highest positioned role with a color set
+ * @param {import('discord.js').Interaction} interaction 
+ * @returns {number | undefined} The position of the highest role with a color set or undefined if no roles with colors exist
+ */
+function getHighestColoredRolePosition(interaction) {
+    let pos;
+    interaction.guild.roles.cache.forEach((role) => {
+        if (role.color !== 0 && (role.position > pos || pos === undefined)) {
+            pos = role.position;
+        }
+    });
+    return pos;
+}
+
 
 
 /**
  * 
  * @param {import('discord.js').Interaction} interaction 
- * @returns 
+ * @returns {number} The position to set the new role
  */
-async function getGreatestRolePosition(interaction) {
-    const maxPosition = await getBotRolePosition(interaction);
-    let position = 1;
-    
-    // Find the highest positioned role with the 'Administrator' permissions that also has a color
-    interaction.guild.roles.cache.forEach((role) => {
-        if (role.position !== 0 && role.color !== 0 && role.permissions.toArray().includes('Administrator') && role.position < maxPosition && role.position+1 > position) {
-            position = role.position+1;
-        }
-    });
-    return position;
+function getNewRolePosition(interaction) {
+    const absoluteMaxPosition = getBotRolePosition(interaction);
+    const preferredPosition = getLowestColorRolePosition(interaction);
+    const highestColoredPosition = getHighestColoredRolePosition(interaction);
+
+    logger.debug(`preferredPosition: ${preferredPosition} | highestColoredPosition: ${highestColoredPosition} | absoluteMaxPosition: ${absoluteMaxPosition}`);
+    logger.debug(`New role position: ${preferredPosition ?? highestColoredPosition ?? absoluteMaxPosition}`);
+
+    return preferredPosition ?? highestColoredPosition ?? absoluteMaxPosition;
 }
 
+/**
+ * 
+ * @param {import('discord.js').Interaction} interaction 
+ * @param {string} message
+ * @returns 
+ */
 async function cancelInteraction(interaction, message) {
     await interaction.editReply({content: `Interaction cancelled ${message}`, embeds: [], components: []});
     return false;
@@ -42,10 +77,8 @@ async function colorHandler(interaction, id) {
 
     const memberRole = await interaction.member.roles.cache.find(role => {return role.name.endsWith(`'s Color`)});
 
-    if (memberRole === undefined) {        
-        const position = await getGreatestRolePosition(interaction);
-        logger.debug(`Creating role ${roleName} for ${interaction.member.id} with initial color #${id.toUpperCase()} at position ${position}`);
-        await interaction.guild.roles.create( {name: roleName, color: hex, permissions: [], position: position} )
+    if (memberRole === undefined) {
+        return await interaction.guild.roles.create( {name: roleName, color: hex, permissions: [], position: getNewRolePosition(interaction)} )
             .then(async role => {
                     await interaction.member.roles.add(role);
                     await interaction.editReply({content: `#${id.toUpperCase()}`, embeds: [{title: 'Your color has been set!', description: `${role} is your color\nUse \`/color set\` again to change it`, color: hex}], components: []});
